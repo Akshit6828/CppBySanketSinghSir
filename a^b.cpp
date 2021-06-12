45d67d10589d3371ce798fafaaec2c1967edda02
Aman007-dev <83749965+Aman007-dev@users.noreply.github.com>
2021-06-12 23:09:10 +0530

    add

--- a^b.cpp
+++ a^b.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     //method1- brute force, That is multiplying and updating in a loop
-    //using dividing power upto they break down to 0 or 1
+    //method3(most optimised)- using dividing power upto they break down to 1 (Here used iterative can do it in recursive way also)
     int t;
     cin>>t;
     while(t--){
@@ -19,3 +19,9 @@ int main(){
     }
     return 0;
 }
+//method2- Recursion, 
+//Base case- if b=0  then result=1
+//Recursive intution- If we assume we can calculate a^b from a function f(a,b) then lets call f(a,b-1) which makes it recursive
+//Self work- return a*f(a,b-1);
+//Time complexity- O(b) 
+//Space complexity- O(b)   In recursive functions memory we call space complexity is the maximum space that function can take...and here function takes n call stack memory
